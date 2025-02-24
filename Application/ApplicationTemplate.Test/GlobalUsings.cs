129b79078b1fd8d1266843360192899fef31a893
Prabath Maduranga Peiris <prabathSL@outlook.com>
2025-02-24 18:33:43 +0800

    added middleware tests

--- Application/ApplicationTemplate.Test/GlobalUsings.cs
+++ Application/ApplicationTemplate.Test/GlobalUsings.cs
@@ -5,7 +5,14 @@ global using ApplicationTemplate.Infrastructure;
 global using ApplicationTemplate.API.Application.Queries;
 global using ApplicationTemplate.API.Controllers;
 global using ApplicationTemplate.API.Application.Validations;
+global using ApplicationTemplate.API.Application.Models;
+global using ApplicationTemplate.API.Extentions.ExtentionHelpers;
 
+global using System.Net;
+global using FluentValidation;
+global using FluentValidation.Results;
+global using Microsoft.AspNetCore.Http;
+global using Newtonsoft.Json;
 global using FluentValidation.TestHelper;
 global using MediatR;
 global using Microsoft.AspNetCore.Mvc;
